2b7eb450ac1021a026efd6aaf9ed6affbbd6836b
Ahmed Missouri <ahmedmissouri@icloud.com>
2025-03-24 08:30:46 +0000

    Initial Commit

---
+++ Learn-Frameworks/Learn_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_FrameworksApp.swift
+//  Learn-Frameworks
+//
+//  Created by Ahmed Missouri on 24/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Learn_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
